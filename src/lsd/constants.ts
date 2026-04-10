271abec6b83404b5cbc538d01c9e56de55fc8c26
naturexie <786281870@qq.com>
2026-04-10 21:33:39 +0800

    feat: add config

---
+++ src/lsd/constants.ts
@@ -0,0 +1,13 @@
+export const BSC_CHAIN_ID = "0x38";
+
+export const BSC_USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
+export const BSC_LSD_USDT_ADDRESS =
+  "0x5382555840ef9f54ef6d3ee5da60f12bcabf4b87";
+
+export const BSC_USDT_DECIMALS = 18;
+export const LSD_USDT_DECIMALS = 18;
+
+export const LSD_CONTRACT_ID = "lsd-usdt.rhealab.near";
+export const BURROW_CONTRACT_ID = "contract.main.burrow.near";
+export const NEAR_USDT_ADDRESS = "usdt.tether-token.near";
+export const NEAR_USDT_DECIMALS = 6;
